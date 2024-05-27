17eee2d6a01c31a92e0e4c99c59f66908aa26ea4
QuentinJGMace <quentinjg.mace@gmail.com>
2024-05-27 12:46:58 +0200

    dataset

---
+++ dataset/__init__.py
@@ -0,0 +1,1 @@
+from .utils import build_data_loader
---
+++ dataset/utils.py
@@ -0,0 +1,357 @@
+from collections import defaultdict
+
+import os
+import random
+import json
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as T
+from PIL import Image
+
+
+def read_image(path):
+    """
+    Read an image from a file path and return it in the form of a PIL image.
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File not found: {path}")
+
+    try:
+        img = Image.open(path)
+        return img
+    except Exception as e:
+        print(f"Error reading image {path}: {e}")
+
+
+def read_json(path):
+    """
+    Read a JSON file from a file path and return it as a dictionary.
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File not found: {path}")
+
+    try:
+        with open(path, "r") as f:
+            data = json.load(f)
+        return data
+    except Exception as e:
+        print(f"Error reading JSON file {path}: {e}")
+
+
+def write_json(data, path):
+    """
+    Write a dictionary to a JSON file.
+    """
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4, separators=(",", ": "))
+
+
+class Datum:
+    """
+    A class to store a single data point.
+
+    Args:
+        image_path (str): The path to the image file.
+        label (int): The label of the image.
+        domain (int): The domain of the image.
+        classname (str): The class name of the image.
+    """
+
+    def __init__(self, impath="", label=0, domain=-1, classname=""):
+        assert isinstance(impath, str)
+        assert isinstance(label, int)
+        assert isinstance(domain, int)
+        assert isinstance(classname, str)
+
+        self._impath = impath
+        self._label = label
+        self._domain = domain
+        self._classname = classname
+
+    @property
+    def impath(self):
+        return self._impath
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def domain(self):
+        return self._domain
+
+    @property
+    def classname(self):
+        return self._classname
+
+
+class DatasetBase:
+    """
+    Dataset class for domain adapatation, domain generalization, and ssl.
+    """
+
+    def __init__(
+        self, train_x=None, train_u=None, val=None, test=None, classnames=None
+    ):
+        self._train_x = train_x
+        self._train_u = train_u
+        self._val = val
+        self._test = test
+
+        self._num_classes = self.get_num_classes(train_x)
+        self._lab2cname, self._classnames = self.get_lab2cname(train_x)
+
+    @property
+    def train_x(self):
+        return self._train_x
+
+    @property
+    def train_u(self):
+        return self._train_u
+
+    @property
+    def val(self):
+        return self._val
+
+    @property
+    def test(self):
+        return self._test
+
+    @property
+    def lab2cname(self):
+        return self._lab2cname
+
+    @property
+    def classnames(self):
+        return self._classnames
+
+    @property
+    def num_classes(self):
+        return self._num_classes
+
+    def get_num_classes(self, data_source):
+        """Count number of classes.
+
+        Args:
+            data_source (list): a list of Datum objects.
+        """
+        label_set = set()
+        for item in data_source:
+            label_set.add(item.label)
+        return max(label_set) + 1
+
+    def get_lab2cname(self, data_source):
+        """Get a label-to-classname mapping (dict).
+
+        Args:
+            data_source (list): a list of Datum objects.
+        """
+        container = set()
+        for item in data_source:
+            container.add((item.label, item.classname))
+        mapping = {label: classname for label, classname in container}
+        labels = list(mapping.keys())
+        labels.sort()
+        classnames = [mapping[label] for label in labels]
+        return mapping, classnames
+
+    def check_input_domains(self, source_domains, target_domains):
+        self.is_input_domain_valid(source_domains)
+        self.is_input_domain_valid(target_domains)
+
+    def is_input_domain_valid(self, input_domains):
+        for domain in input_domains:
+            if domain not in self.domains:
+                raise ValueError(
+                    "Input domain must belong to {}, "
+                    "but got [{}]".format(self.domains, domain)
+                )
+
+    def download_data(self, url, dst, from_gdrive=True):
+        if not os.path.exists(os.path.dirname(dst)):
+            os.makedirs(os.path.dirname(dst))
+
+        if from_gdrive:
+            gdown.download(url, dst, quiet=False)
+        else:
+            raise NotImplementedError
+
+        print("Extracting file ...")
+
+        try:
+            tar = tarfile.open(dst)
+            tar.extractall(path=os.path.dirname(dst))
+            tar.close()
+        except:
+            zip_ref = zipfile.ZipFile(dst, "r")
+            zip_ref.extractall(os.path.dirname(dst))
+            zip_ref.close()
+
+        print("File extracted to {}".format(os.path.dirname(dst)))
+
+    def generate_fewshot_dataset(self, *data_sources, num_shots=-1, repeat=True):
+        """Generate a few-shot dataset (typically for the training set).
+
+        This function is useful when one wants to evaluate a model
+        in a few-shot learning setting where each class only contains
+        a few number of images.
+
+        Args:
+            data_sources: each individual is a list containing Datum objects.
+            num_shots (int): number of instances per class to sample.
+            repeat (bool): repeat images if needed.
+        """
+        if num_shots < 1:
+            if len(data_sources) == 1:
+                return data_sources[0]
+            return data_sources
+
+        print(f"Creating a {num_shots}-shot dataset")
+
+        output = []
+
+        for data_source in data_sources:
+            tracker = self.split_dataset_by_label(data_source)
+            dataset = []
+
+            for label, items in tracker.items():
+                if len(items) >= num_shots:
+                    sampled_items = random.sample(items, num_shots)
+                else:
+                    if repeat:
+                        sampled_items = random.choices(items, k=num_shots)
+                    else:
+                        sampled_items = items
+                dataset.extend(sampled_items)
+
+            output.append(dataset)
+
+        if len(output) == 1:
+            return output[0]
+
+        return output
+
+    def split_dataset_by_label(self, data_source):
+        """Split a dataset, i.e. a list of Datum objects,
+        into class-specific groups stored in a dictionary.
+
+        Args:
+            data_source (list): a list of Datum objects.
+        """
+        output = defaultdict(list)
+
+        for item in data_source:
+            output[item.label].append(item)
+
+        return output
+
+    def split_dataset_by_domain(self, data_source):
+        """Split a dataset, i.e. a list of Datum objects,
+        into domain-specific groups stored in a dictionary.
+
+        Args:
+            data_source (list): a list of Datum objects.
+        """
+        output = defaultdict(list)
+
+        for item in data_source:
+            output[item.domain].append(item)
+
+        return output
+
+
+class DatasetWrapper(Dataset):
+    """A wrapper class for a dataset.
+
+    Args:
+        data_source (list): a list of Datum objects.
+        input_size (int or tuple): the size of the input image.
+        transform (callable): a function/transform to preprocess the image.
+        is_train (bool): whether the dataset is used for training.
+        return_img0 (bool): whether to return the original image.
+        k_tfm (int): the number of times to augment an image.
+    """
+
+    def __init__(
+        self,
+        data_source,
+        input_size,
+        transform=None,
+        is_train=False,
+        return_img0=False,
+        k_tfm=1,
+    ):
+        self.data_source = data_source
+        self.transform = transform  # accept list (tuple) as input
+        self.is_train = is_train
+        # Augmenting an image K>1 times is only allowed during training
+        self.k_tfm = k_tfm if is_train else 1
+        self.return_img0 = return_img0
+
+        to_tensor = []
+        to_tensor += [T.ToTensor()]
+        self.to_tensor = T.Compose(to_tensor)
+
+    def __len__(self):
+        return len(self.data_source)
+
+    def __getitem__(self, idx):
+        item = self.data_source[idx]
+
+        output = {"label": item.label, "domain": item.domain, "impath": item.impath}
+        img0 = read_image(item.impath)
+        img = self.transform(img0)
+        output["img"] = img
+
+        return output["img"], output["label"]
+
+
+def build_data_loader(
+    data_source=None,
+    batch_size=64,
+    input_size=224,
+    tfm=None,
+    is_train=True,
+    shuffle=False,
+    dataset_wrapper=None,
+):
+
+    if dataset_wrapper is None:
+        dataset_wrapper = DatasetWrapper
+
+    # Build data loader
+    data_loader = torch.utils.data.DataLoader(
+        dataset_wrapper(
+            data_source, input_size=input_size, transform=tfm, is_train=is_train
+        ),
+        batch_size=batch_size,
+        num_workers=0,
+        shuffle=shuffle,
+        drop_last=False,
+        pin_memory=(torch.cuda.is_available()),
+    )
+    assert len(data_loader) > 0
+
+    return data_loader
+
+
+def listdir_nohidden(path, sort=False):
+    """List non-hidden items in a directory.
+
+    Args:
+         path (str): directory path.
+         sort (bool): sort the items.
+    """
+    items = [f for f in os.listdir(path) if not f.startswith(".") and "sh" not in f]
+    if sort:
+        items.sort()
+    return items
+
+
+def collate_fn(batch):
+    return {
+        "pixel_values": torch.stack([x["pixel_values"] for x in batch]),
+        "labels": torch.tensor([x["labels"] for x in batch]),
+    }
