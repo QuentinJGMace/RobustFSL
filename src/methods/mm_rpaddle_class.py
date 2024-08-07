d5680f3df7351b0ea43babe821eb68e6dd0dbf89
QuentinJGMace <quentinjg.mace@gmail.com>
2024-08-07 17:09:31 +0200

    theta class dependant methods

---
+++ src/methods/mm_rpaddle_class.py
@@ -0,0 +1,128 @@
+import time
+
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+from src.methods.utils import get_one_hot, simplex_project, Glasso_cov_estimate
+from src.methods.abstract_method import AbstractMethod, MinMaxScaler
+from src.methods.rpaddle_base import RPADDLE_base
+from src.methods.mm_rapddle_id import MM_PADDLE_id
+
+
+class MM_PADDLE_class(MM_PADDLE_id):
+    def __init__(self, backbone, device, log_file, args):
+        super().__init__(backbone=backbone, device=device, log_file=log_file, args=args)
+
+    def init_params(self, support, y_s, query):
+        n_tasks, n_query, feature_dim = query.size()
+        _, n_support, _ = support.size()
+
+        self.init_prototypes(support, y_s)
+        self.theta = torch.ones(n_tasks, n_support + n_query, self.n_class).to(
+            self.device
+        )
+        self.u = (self.get_logits(query)).softmax(-1).to(self.device)
+
+        if not self.id_cov:
+            # self.q strores the n_class covariances
+            self.q = (
+                torch.eye(feature_dim)
+                .unsqueeze(0)
+                .repeat(n_tasks, self.n_class, 1, 1)
+                .to(self.device)
+            )
+
+    def compute_mahalanobis(self, samples, theta):
+        """
+        Computes the Mahalanobis distance between the samples and the prototypes
+        inputs:
+            samples : torch.Tensor of shape [n_task, n_sample, feature_dim]
+            theta : torch.Tensor of shape [n_task, n_sample, n_class]
+        """
+        if not self.id_cov:
+            tranformed_samples = torch.matmul(
+                self.q.unsqueeze(1), samples.unsqueeze(2).unsqueeze(-1)
+            ).squeeze(
+                -1
+            )  # Q_kx_n [n_task, n_sample, n_class, feature_dim]
+        else:
+            tranformed_samples = samples.unsqueeze(2)
+        # computes the norm of the difference between the samples and the prototypes
+        dist_beta = (tranformed_samples - self.prototypes.unsqueeze(1)).norm(
+            dim=-1
+        ) ** self.beta  # ||Q_kx_n - prototype_k||^beta [n_task, n_sample, n_class]
+        dist_beta /= theta ** (
+            self.beta - 1
+        )  # ||Q_kx_n - prototype_k||^beta/theta [n_task, n_sample, n_class]
+
+        return dist_beta
+
+    def update_u(self, samples_query, class_prop):
+        """
+        Updates the soft labels u
+
+        Args:
+            samples : torch.Tensor of shape [n_task, n_query, feature_dim]
+            class_prop : torch.Tensor of shape [n_task, n_class]
+
+        Updates:
+            self.u : torch.Tensor of shape [n_task, n_query, n_class]
+
+        Returns:
+            None
+        """
+        n_query = samples_query.size(1)
+        feature_dim = samples_query.size(2)
+        arg_softmax = (
+            (
+                (-1 / 2)
+                * self.rho_beta(samples_query)
+                / (self.theta[:, -n_query:] ** (self.beta - 1))
+            )
+            + ((self.lambd / n_query) * (torch.log(class_prop.unsqueeze(1))))
+            - feature_dim * (1 - 1 / self.beta) * torch.log(self.theta[:, -n_query:])
+        )
+        self.u = arg_softmax.softmax(-1)
+
+    def update_theta(self, samples, all_u):
+        """
+        Updates the theta parameters
+
+        Args:
+            samples : torch.Tensor of shape [n_task, n_support + n_query, feature_dim]
+
+        Updates:
+            self.theta : torch.Tensor of shape [n_task, n_support + n_query, n_class]
+
+        Returns:
+            None
+        """
+        _, _, feature_dim = samples.size()
+        if not self.change_theta_reg:
+            self.theta = (
+                (self.beta - 1)
+                / 2
+                * 1
+                / (feature_dim * (1 - 1 / self.beta) - self.kappa)
+                * ((self.rho_beta(samples)))
+            ) ** (1 / (self.beta - 1))
+        else:
+            self.theta = (
+                (self.beta - 1)
+                / 2
+                * (self.eta**self.p)
+                / self.p
+                * ((self.rho_beta(samples)))
+            ) ** (1 / (self.beta - 1))
+
+    def update_prot(self, samples, all_u):
+        n_tasks, n_samples, _ = samples.size()
+        rho = self.rho(samples).unsqueeze(-1)
+        den_den = (rho ** (1 - self.beta / 2)) * (self.theta.unsqueeze(3)) ** (
+            self.beta - 1
+        )
+        num = ((all_u.unsqueeze(-1) * samples.unsqueeze(2)) / den_den).sum(dim=1)
+
+        den = (all_u.unsqueeze(-1) / den_den).sum(dim=1)
+
+        self.prototypes = num / den
--- src/methods/rpaddle_gd_class.py
+++ src/methods/rpaddle_gd_class.py
@@ -1,43 +1,67 @@
+"""Rapaddle implementation with SGD, theta also varies on the support set contrary to the original implementation"""
+
 import time
 
 import torch
-import torch.nn.functional as F
 from tqdm import tqdm
-from src.methods.utils import simplex_project
+import torch.nn.functional as F
+from src.methods.utils import get_one_hot, simplex_project
 from src.methods.rpaddle_base import RPADDLE_base
 
 
-class MutlNoisePaddle_GD(RPADDLE_base):
+class MultNoisePaddle_GD2_class(RPADDLE_base):
     def __init__(self, backbone, device, log_file, args):
         super().__init__(backbone=backbone, device=device, log_file=log_file, args=args)
 
     def init_params(self, support, y_s, query):
-        """
-        Initializes the parameters
-        inputs:
-            support : torch.Tensor of shape [n_task, shot, feature_dim]
-            y_s : torch.Tensor of shape [n_task, shot]
-            query : torch.Tensor of shape [n_task, n_query, feature_dim]
-        """
         n_tasks, n_query, feature_dim = query.size()
+        _, n_support, _ = support.size()
 
         self.init_prototypes(support, y_s)
-        self.theta = torch.nn.Parameter(torch.ones(n_tasks, n_query).to(self.device))
-        self.u = torch.nn.Parameter(
-            (self.get_logits(query)).softmax(-1).to(self.device)
+        self.theta = torch.ones(n_tasks, n_support + n_query, self.n_class).to(
+            self.device
         )
-        self.prototypes = torch.nn.Parameter(self.prototypes)
+        self.u = (self.get_logits(query)).softmax(-1).to(self.device)
 
         if not self.id_cov:
             # self.q strores the n_class covariances
-            self.q = torch.nn.Parameter(
-                (
-                    torch.eye(feature_dim)
-                    .unsqueeze(0)
-                    .repeat(n_tasks, self.n_class, 1, 1)
-                    .to(self.device)
-                )
+            self.q = (
+                torch.eye(feature_dim)
+                .unsqueeze(0)
+                .repeat(n_tasks, self.n_class, 1, 1)
+                .to(self.device)
             )
+
+            self.q = torch.nn.Parameter(self.q)
+
+        self.prototypes = torch.nn.Parameter(self.prototypes)
+        self.theta = torch.nn.Parameter(self.theta)
+        self.u = torch.nn.Parameter(self.u)
+
+    def compute_mahalanobis(self, samples, theta):
+        """
+        Computes the Mahalanobis distance between the samples and the prototypes
+        inputs:
+            samples : torch.Tensor of shape [n_task, n_sample, feature_dim]
+            theta : torch.Tensor of shape [n_task, n_sample, n_class]
+        """
+        if not self.id_cov:
+            tranformed_samples = torch.matmul(
+                self.q.unsqueeze(1), samples.unsqueeze(2).unsqueeze(-1)
+            ).squeeze(
+                -1
+            )  # Q_kx_n [n_task, n_sample, n_class, feature_dim]
+        else:
+            tranformed_samples = samples.unsqueeze(2)
+        # computes the norm of the difference between the samples and the prototypes
+        dist_beta = (tranformed_samples - self.prototypes.unsqueeze(1)).norm(
+            dim=-1
+        ) ** self.beta  # ||Q_kx_n - prototype_k||^beta [n_task, n_sample, n_class]
+        dist_beta /= theta ** (
+            self.beta - 1
+        )  # ||Q_kx_n - prototype_k||^beta/theta [n_task, n_sample, n_class]
+
+        return dist_beta
 
     def run_method(self, support, query, y_s, y_q):
         """
@@ -47,37 +71,44 @@ class MutlNoisePaddle_GD(RPADDLE_base):
             y_s : torch.Tensor of shape [n_task, shot]
             y_q : torch.Tensor of shape [n_task, n_query]
         """
+        # self.logger.info(
+        #     " ==> Executing RobustPADDLE with LAMBDA = {}".format(self.lambd)
+        # )
+
         t0 = time.time()
         y_s_one_hot = F.one_hot(y_s, self.n_class).to(self.device)
 
         self.init_params(support, y_s, query)
         if not self.id_cov:
-            optimizer = torch.optim.SGD(
+            optimizer = torch.optim.Adam(
                 [self.prototypes, self.theta, self.u, self.q], lr=self.lr
             )
         else:
-            optimizer = torch.optim.SGD(
+            optimizer = torch.optim.Adam(
                 [self.prototypes, self.theta, self.u], lr=self.lr
             )
 
         all_samples = torch.cat([support.to(self.device), query.to(self.device)], 1)
-        theta_support = torch.ones(support.size(0), support.size(1)).to(self.device)
 
         feature_dim = all_samples.size(-1)
+
+        n_tasks = query.size(0)
+
+        losses = []
         for i in tqdm(range(self.n_iter)):
 
-            prototypes_old = self.prototypes.clone()
-            theta_old = self.theta.clone()
+            prototypes_old = self.prototypes.detach().clone()
+            theta_old = self.theta.detach().clone()
+            u_old = self.u.detach().clone()
+
             if not self.id_cov:
-                q_old = self.q.clone()
+                q_old = self.q.detach().clone()
             else:
                 q_old = None
-            u_old = self.u.clone()
             t0 = time.time()
-            all_theta = torch.cat([theta_support, self.theta], 1)
 
             # Data fitting term
-            distances = self.compute_mahalanobis(all_samples, all_theta)
+            distances = self.compute_mahalanobis(all_samples, self.theta)
             all_p = torch.cat([y_s_one_hot.float(), self.u.float()], dim=1)
 
             data_fitting = (
@@ -86,10 +117,20 @@ class MutlNoisePaddle_GD(RPADDLE_base):
 
             # Theta regularization term
             sum_log_theta = (
-                feature_dim * (1 - 1 / self.beta) - self.kappa
-            ) * torch.log(self.theta + 1e-12).sum(1).mean(0)
+                (
+                    all_p
+                    * (feature_dim * (1 - 1 / self.beta) - self.kappa)
+                    * torch.log(self.theta + 1e-12)
+                )
+                .sum(dim=(1, 2))
+                .mean(0)
+            )
             l2_theta = (
-                1 / self.eta * (self.theta.norm(dim=-1, keepdim=False)) ** 2
+                1
+                / self.n_class
+                * (1 / self.eta)
+                * (self.theta.reshape(n_tasks, -1).norm(dim=-1, keepdim=False, p=2))
+                ** 2
             ).mean(0)
 
             theta_term = sum_log_theta + l2_theta
@@ -118,13 +159,16 @@ class MutlNoisePaddle_GD(RPADDLE_base):
                 - self.lambd * partition_complexity
                 + theta_term
                 - q_term
-                + ent_barrier  # - self.lambd * partition_complexity + theta_term - q_term
+                # + ent_barrier  # - self.lambd * partition_complexity + theta_term - q_term
             )
 
             # Backward pass
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+            losses.append(loss.item())
+
             # Projection into simplex
             with torch.no_grad():
                 self.u.data = simplex_project(self.u, device=self.device)
@@ -138,4 +182,6 @@ class MutlNoisePaddle_GD(RPADDLE_base):
             )
             self.record_convergence(timestamp=t_end - t0, criterions=criterions)
 
+        # plt.plot(losses)
+        # plt.savefig("losses.png")
         self.record_acc(y_q=y_q)
