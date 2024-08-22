3ca6a94e919875698b8839560988c5b8c329b454
QuentinJGMace <quentinjg.mace@gmail.com>
2024-08-22 17:59:09 +0200

    new methods + theta tracking

--- src/methods/__init__.py
+++ src/methods/__init__.py
@@ -2,12 +2,15 @@ from .rpaddle_gd import MutlNoisePaddle_GD
 from .rpaddle_gd2 import MultNoisePaddle_GD2
 from .rpaddle_gd_class import MultNoisePaddle_GD2_class
 from .mm_rapddle_id import MM_PADDLE_id
+from .mm_rpaddle_reg import MM_RPADDLE_reg
 from .mm_rpaddle_class import MM_PADDLE_class
+from .mm_rpaddle_reg_diag_cov import MM_RPADDLE_reg_sigmas
 from .em_rfsl import EM_RobustPaddle_ID
 from .em_rfsl_cov import EM_RobustPaddle
 from .paddle import Paddle
 from .paddle_gd import Paddle_GD
 from .tim import TIM_GD, Alpha_TIM
+from .rtim import RTIM_GD
 
 
 def get_method_builder(backbone, device, args, log_file):
@@ -26,16 +29,22 @@ def get_method_builder(backbone, device, args, log_file):
         method_builder = MultNoisePaddle_GD2(**method_info)
     elif args.name_method == "RPADDLE2_CLASS":
         method_builder = MultNoisePaddle_GD2_class(**method_info)
+    elif args.name_method == "MM_RPADDLE_REG":
+        method_builder = MM_RPADDLE_reg(**method_info)
     elif args.name_method == "MM_RPADDLE_ID":
         method_builder = MM_PADDLE_id(**method_info)
     elif args.name_method == "MM_RPADDLE_CLASS":
         method_builder = MM_PADDLE_class(**method_info)
+    elif args.name_method == "MM_RPADDLE_REG_SIGMAS":
+        method_builder = MM_RPADDLE_reg_sigmas(**method_info)
     elif args.name_method == "PADDLE":
         method_builder = Paddle(**method_info)
     elif args.name_method == "PADDLE_GD":
         method_builder = Paddle_GD(**method_info)
     elif args.name_method == "TIM_GD":
         method_builder = TIM_GD(**method_info)
+    elif args.name_method == "RTIM_GD":
+        method_builder = RTIM_GD(**method_info)
     elif args.name_method == "ALPHA_TIM":
         method_builder = Alpha_TIM(**method_info)
     elif args.name_method == "EM-PADDLE-ID":
